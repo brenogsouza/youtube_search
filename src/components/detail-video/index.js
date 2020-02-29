b3cf03dcdff05a496301f148954abce949d3656d
brenogsouza <breno.souza.developer@gmail.com>
2020-02-29 15:24:16 -0300

    excluido componentes gerado pelo react-app e criado functional components

---
+++ src/components/detail-video/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const DetailVideo = () => {
+  return (
+    <div>
+      <h1>DetailVideo</h1>
+    </div>
+  );
+};
+export default DetailVideo;
---
+++ src/components/list-video/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ListVideo = () => {
+  return (
+    <div>
+      <h1>ListVideo</h1>
+    </div>
+  );
+};
+
+export default ListVideo;
